870116362bea3ac6a6567148ae9753db6259f587
Chad Walpole <ccwalpole@gmail.com>
2025-09-20 12:47:10 -0600

    update port and seed file

---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+// prisma.config.ts
+import path from "node:path";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: path.join("prisma", "schema.prisma"),
+  migrations: {
+    path: path.join("prisma", "migrations"),
+    // 👇 put your seed command here
+    seed: "node prisma/seed.mjs",
+    // If you use TS: seed: "tsx prisma/seed.ts"
+  },
+});
